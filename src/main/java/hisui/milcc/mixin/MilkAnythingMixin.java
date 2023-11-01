0a71dafcee6c3918c48b508088ab60a29c6634d8
hiisuuii <dogglez7777@gmail.com>
2023-10-31 19:32:53 -0600

    blah

--- src/main/java/hisui/milcc/mixin/MilkAnythingMixin.java
+++ src/main/java/hisui/milcc/mixin/MilkAnythingMixin.java
@@ -15,6 +15,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,9 +34,16 @@ abstract class MilkAnythingMixin extends LivingEntity {
 		ItemStack itemStack = player.getStackInHand(hand);
 		if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
 			player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0f, 1.0f);
-			ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, Items.MILK_BUCKET.getDefaultStack());
-			NbtList lore = itemStack2.getOrCreateSubNbt("display").getList("Lore", NbtElement.STRING_TYPE);
-			lore.add(NbtString.of(this.getName().getString()));
+			ItemStack outputStack = new ItemStack(Items.MILK_BUCKET);
+
+			NbtList lore = outputStack.getOrCreateSubNbt("display").getList("Lore", NbtElement.STRING_TYPE);
+			lore.add(NbtString.of(Text.Serializer.toJson(Text.empty().append(this.getName()).setStyle(Style.EMPTY.withColor(Formatting.GRAY)))));
+			lore.add(NbtString.of("Hello"));
+			outputStack.getOrCreateSubNbt("display").put("Lore", lore);
+
+			ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, outputStack);
+
+
 			player.setStackInHand(hand, itemStack2);
 			cir.setReturnValue(ActionResult.success(this.getWorld().isClient));
 		}
