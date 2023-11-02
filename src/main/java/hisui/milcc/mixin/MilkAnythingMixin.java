dd1a6f673ab76ff5bcfdd8fb4a62e3d57cb6b991
hiisuuii <dogglez7777@gmail.com>
2023-11-02 11:17:54 -0600

    change @Inject to @ModifyReturnValue for compat

--- src/main/java/hisui/milcc/mixin/MilkAnythingMixin.java
+++ src/main/java/hisui/milcc/mixin/MilkAnythingMixin.java
@@ -1,5 +1,6 @@
 package hisui.milcc.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
@@ -7,7 +8,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
@@ -20,8 +20,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
 abstract class MilkAnythingMixin extends LivingEntity {
@@ -29,23 +27,26 @@ abstract class MilkAnythingMixin extends LivingEntity {
 		super(entityType, world);
 	}
 
-	@Inject(at = @At("HEAD"), method = "interactMob", cancellable = true)
-	private void milcc$milkAnythingMixin(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-		ItemStack itemStack = player.getStackInHand(hand);
-		if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
-			player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0f, 1.0f);
-			ItemStack outputStack = new ItemStack(Items.MILK_BUCKET);
+	@ModifyReturnValue(at = @At("HEAD"), method = "interactMob")
+	private ActionResult milcc$milkAnythingMixin(ActionResult original, PlayerEntity player, Hand hand) {
+		if(original == ActionResult.PASS) {
+			ItemStack itemStack = player.getStackInHand(hand);
+			if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
+				player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0f, 1.0f);
+				ItemStack outputStack = new ItemStack(Items.MILK_BUCKET);
 
-			NbtList lore = outputStack.getOrCreateSubNbt("display").getList("Lore", NbtElement.STRING_TYPE);
-			lore.add(NbtString.of(Text.Serializer.toJson(Text.empty().append(this.getName()).setStyle(Style.EMPTY.withColor(Formatting.GRAY)))));
-			lore.add(NbtString.of("Hello"));
-			outputStack.getOrCreateSubNbt("display").put("Lore", lore);
+				NbtList lore = outputStack.getOrCreateSubNbt("display").getList("Lore", NbtElement.STRING_TYPE);
+				lore.add(NbtString.of(Text.Serializer.toJson(Text.empty().append(this.getName()).setStyle(Style.EMPTY.withColor(Formatting.GRAY)))));
+				lore.add(NbtString.of("Hello"));
+				outputStack.getOrCreateSubNbt("display").put("Lore", lore);
 
-			ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, outputStack);
+				ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, outputStack);
 
 
-			player.setStackInHand(hand, itemStack2);
-			cir.setReturnValue(ActionResult.success(this.getWorld().isClient));
+				player.setStackInHand(hand, itemStack2);
+				return ActionResult.success(this.getWorld().isClient());
+			}
 		}
+		return original;
 	}
 }
